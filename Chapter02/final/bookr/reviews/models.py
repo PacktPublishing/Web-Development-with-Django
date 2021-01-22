dea1c81577fe9f4aa68f5163b849fa456f7634ec
saumyajha09 <57439483+saumyajha09@users.noreply.github.com>
2021-01-22 18:14:21 +0530

    Update models.py

--- Chapter02/final/bookr/reviews/models.py
+++ Chapter02/final/bookr/reviews/models.py
@@ -56,7 +56,7 @@ class BookContributor(models.Model):
 
 class Review(models.Model):
     content = models.TextField(help_text="The Review text.")
-    rating = models.IntegerField(help_text="The the reviewer has given.")
+    rating = models.IntegerField(help_text="The rating the reviewer has given.")
     date_created = models.DateTimeField(auto_now_add=True,
                                         help_text="The date and time the review was created.")
     date_edited = models.DateTimeField(null=True,
